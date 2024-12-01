fd6e6e127f291a1632b6cd0b10dcda028ee75b41
TomFirth <tom.firth84@gmail.com>
2024-12-01 11:22:12 +0000

    day 1, part 1

--- 2024/1.ts
+++ 2024/1.ts
@@ -1,6 +1,37 @@
-import * as fs from "fs";
-const data: string = fs.readFileSync('1.txt', 'utf-8');
+import * as fs from 'fs';
+import * as readline from 'readline';
 
-// add each side of the list to an array
-// sort each array
-// measure the difference between the two (right - left)
+const createColumns = async (data: string): Promise<{ arrayA: number[]; arrayB: number[] }>  => {
+  let arrayA: number[] = [];
+  let arrayB: number[] = [];
+  const fileStream = fs.createReadStream(data);
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity,
+  });
+
+  for await (const line of rl) {
+    const [a, b] = line.split(/\s+/).map(Number);
+    arrayA.push(Number(a));
+    arrayB.push(Number(b));
+  }
+  return { arrayA, arrayB };
+}
+
+createColumns('./1.txt')
+  .then(({ arrayA: a, arrayB: b }) => {
+    return {
+      a: a.map(Number).sort((x, y) => x - y),
+      b: b.map(Number).sort((x, y) => x - y)
+    }
+  })
+  .then(({ a, b }) => {
+    let total: number = 0;
+    for (let i = 0; i < a.length; i++) {
+      let left = a[i];
+      let right = b[i]
+      if (left > right) total += left - right;
+      else total += right - left;
+    }
+    console.log("Day 1, Part 1:", total);
+  });
