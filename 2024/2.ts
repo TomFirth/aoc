d0e712895a580fee1286b85322da8c6bf1cbb7b9
TomFirth <tom.firth84@gmail.com>
2024-12-03 11:22:10 +0000

    Day 2

--- 2024/2.ts
+++ 2024/2.ts
@@ -1,19 +1,41 @@
 import * as fs from "fs";
-const data: string = fs.readFileSync('2.txt', 'utf-8');
+const data: string = fs.readFileSync("2.txt", "utf-8");
+
+type Levels = number[];
 
 let totalA = 0;
+let totalB = 0;
 
-data.split(/\r?\n/).map(report => {
-  const levels = report.split(" ").map(Number);
-  const withinRange = levels.every((level, i) => 
-    i === 0 || (Math.abs(level - levels[i - 1]) <= 3 && Math.abs(level - levels[i - 1]) >= 1)
-  );
+const isValidSequence = (levels: Levels): boolean => {
+  for (let i = 0; i < levels.length; i++) {
+    const modifiedArr: Levels = [...levels.slice(0, i), ...levels.slice(i + 1)];
+    if (checkSequence(modifiedArr)) return true;
+  }
+  return false;
+}
 
-  const isIncreasing = levels.every((level, i) => i === 0 || level >= levels[i - 1]);
-  const isDecreasing = levels.every((level, i) => i === 0 || level <= levels[i - 1]);
+const checkSequence = (levels: Levels): boolean => {
+  const isIncreasing = levels[0] < levels[levels.length - 1];
 
-  const isSafe = withinRange && (isIncreasing !== isDecreasing);
-  if (isSafe) totalA++;
+  for (let i = 1; i < levels.length; i++) {
+    const diff = isIncreasing
+      ? levels[i] - levels[i - 1]
+      : levels[i - 1] - levels[i];
+
+    if (diff < 1 || diff > 3) return false;
+  }
+  return true;
+}
+
+data.trim().split(/\r?\n/).map(report => {
+  const levels: Levels = report.split(" ").map(Number);
+
+  if (checkSequence(levels)) { 
+    totalA++; 
+    totalB++; 
+  }
+  else if (isValidSequence(levels)) totalB++;
 });
 
 console.log("Day 2, Part 1:", totalA);
+console.log("Day 2, Part 2:", totalB);
